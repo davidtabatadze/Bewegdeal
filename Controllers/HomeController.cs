002ee97aefbd4077b6e3df237675141793793392
David Tabatadze <david.tabatadze@optio.ai>
2026-05-06 16:24:23 +0400

    Make blank.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-using Bewegdeal.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace Bewegdeal.Controllers
 {
@@ -9,17 +7,6 @@ namespace Bewegdeal.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bewegdeal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
