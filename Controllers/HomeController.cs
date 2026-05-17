4e9716467095726cd0a953ab73bcc985cf64aaaf
David Tabatadze <david.tabatadze@optio.ai>
2026-05-17 23:12:44 +0400

    Fix hiw navigation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,27 @@
+using Bewegdeal.Data.Filters;
+using Bewegdeal.Data.Repositories.Abstractions;
+using Bewegdeal.Enums;
 using Bewegdeal.Filters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bewegdeal.Controllers
 {
     [RequireLogin]
-    public class HomeController : Controller
+    public class HomeController(IUserRepository userRepository) : Controller
     {
-        public IActionResult Index() => RedirectToAction("Index", "Dashboard");
+        public async Task<IActionResult> Index()
+        {
+            if (long.TryParse(HttpContext.Session.GetString("UserId"), out var userId))
+            {
+                var user = await userRepository.Get(new UserFilter { Id = userId });
+                if (user is not null && !user.AcquaintedHIW && user.Role != UserRoleEnum.Administrator)
+                {
+                    var action = user.Role == UserRoleEnum.Customer ? "Customer" : "Company";
+                    return RedirectToAction(action, "HowItWorks");
+                }
+            }
+
+            return RedirectToAction("Index", "Dashboard");
+        }
     }
 }
