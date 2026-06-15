d844e527ec82daaa3308ce478532648e60463117
David Tabatadze <david.tabatadze@outlook.com>
2026-06-15 20:13:03 +0400

    Introduce pwa
    * PWA  initial
    
    * Fix sql context
    
    * PWA  initial
    
    * Fix sql context
    
    * Fix vertical menu
    
    * Fix pwa cacheing and logo
    
    ---------
    
    Co-authored-by: Giorgi Tsiklauri <gio.tsiklauri@gmail.com>
    Co-authored-by: David Tabatadze <david.tabatadze@optio.ai>

---
+++ wwwroot/js/pwa.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/sw.js');
+    });
+  }
+})();
