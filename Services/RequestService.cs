595bdd13d1b73a840c91afa13d176906d1278811
David Tabatadze <david.tabatadze@optio.ai>
2026-06-25 00:18:09 +0400

    Fix invoice config

--- Services/RequestService.cs
+++ Services/RequestService.cs
@@ -101,6 +101,7 @@ namespace Bewegdeal.Services
         public async Task<GenericResultModel> Resolve(string number, long userId, decimal? rating)
         {
             var request = await Get(number);
+            var settings = await SettingService.GetCached();
 
             if (request is not null && request.RequesterId == userId && request.Status == RequestStatusEnum.Agreed)
             {
@@ -115,6 +116,9 @@ namespace Bewegdeal.Services
                     new() { RequestId = request.Id, Status = InvoiceStatusEnum.Cancelled }
                 );
 
+                var commision = proposal.Cost / 100 * settings.InvoiceCommissionPersent;
+                var tax = commision / 100 * settings.InvoiceTaxPersent;
+
                 await InvoiceService.Create(new InvoiceEntity
                 {
                     Number = Guid.NewGuid().ToString("N"),
@@ -126,8 +130,8 @@ namespace Bewegdeal.Services
                     CustomerId = request.RequesterId,
                     Currency = proposal.Currency,
                     ServiceCost = proposal.Cost,
-                    SubtotalCost = 100,
-                    TotalCost = 200,
+                    SubtotalCost = commision,
+                    TotalCost = commision + tax,
                     NotificationSent = false,
                     CreateDate = DateTime.Now
                 });
